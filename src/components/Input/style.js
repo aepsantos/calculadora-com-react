2ed1d814c1d41f8597a491aed6330ea245bbfb53
Arthur Emannuel <rufferto__@hotmail.com>
2023-11-29 10:57:45 -0300

    Criação de componentes button e inpu

---
+++ src/components/Input/style.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #D3D3D3;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+    
+    
+
+    input{
+        width: 100%;
+        height: 75px;
+        background-color: #D3D3D3;
+        border: 0;
+        font-size: 24px;
+        font-family: 'Roboto';
+        padding: 0 10px; 
+    }
+`
